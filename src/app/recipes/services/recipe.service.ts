743bfeb55d849685dc7d1e812805852298cb7350
Carlos Salazar <carlos.salazar@slalom.com>
2019-01-28 18:57:29 -0600

    add ingredients to the recipe's

--- src/app/recipes/services/recipe.service.ts
+++ src/app/recipes/services/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,21 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe('creme bruele',
-    'this is a test description',
-    'https://picsum.photos/500/500/?random'),
+      'this is a test description',
+      'https://picsum.photos/500/500/?random',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 12),
+      ],
+    ),
     new Recipe('tart pudding',
-    'this is a test description',
-    'https://picsum.photos/g/500/500/?random')
+      'this is a test description',
+      'https://picsum.photos/g/500/500/?random',
+      [
+        new Ingredient('Bun', 2),
+        new Ingredient('Meat', 1),
+      ],
+    ),
   ];
   constructor() { }
 
