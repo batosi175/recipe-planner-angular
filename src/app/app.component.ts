1bb8679986393f5641c0a2f7f5c5e252d8f8c50b
Carlos Salazar <carlos.salazar@slalom.com>
2019-03-05 09:48:02 -0600

    adding tslint config, prettier, and routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  currentHeader = 'Recipes';
-
-
-  onSelectHeader(selectedHeader: string) {
-    this.currentHeader = selectedHeader;
-  }
-}
+export class AppComponent {}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,12 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  @Output() currentHeader: EventEmitter<string> = new EventEmitter();
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  selectHeader(selectedHeader: string) {
-    this.currentHeader.emit(selectedHeader);
-  }
-
+  ngOnInit() {}
 }
